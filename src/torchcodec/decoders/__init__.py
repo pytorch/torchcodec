d4b011184e37cf5a473414d23fe28d3287549364
Scott Schneider <scottas@meta.com>
2024-06-18 17:01:35 -0700

    [torchcodec] initial version of simple video decoder (#42)
    Summary:
    Pull Request resolved: https://github.com/pytorch-labs/torchcodec/pull/42
    
    Initial version of `SimpleVideoDecoder`. This diff supports:
    1. Creating a simple decoder from file or tensor.
    2. Accessing frames through random access: `decoder[i]`. Internally, this gets a frame by index.
    3. Iterating over all available frames: `for frame in decoder`. Internally, this calls the core library function to get the next frame.
    
    I did not implement slice semantics for random access yet. I think that in order to support that best we'll need to add new capabilities in the core API to support getting a batch of frames from a range. We *could* use `get_frames_by_indices`, but we would end up creating very large lists.
    
    This is a partial implementation of the design: https://fburl.com/gdoc/hxotv2by
    
    Reviewed By: NicolasHug, ahmadsharif1
    
    Differential Revision: D58605628
    
    fbshipit-source-id: b5bda7b0d63ec341608bcf58e82a0c3c8accf923

---
+++ src/torchcodec/decoders/__init__.py
@@ -0,0 +1,1 @@
+from ._simple_video_decoder import SimpleVideoDecoder  # noqa
---
+++ src/torchcodec/decoders/_simple_video_decoder.py
@@ -0,0 +1,62 @@
+import json
+from typing import Union
+
+import torch
+from torchcodec.decoders import _core as core
+
+
+class SimpleVideoDecoder:
+
+    def __init__(self, source: Union[str, bytes, torch.Tensor]):
+        # TODO: support Path objects.
+        if isinstance(source, str):
+            self._decoder = core.create_from_file(source)
+        elif isinstance(source, bytes):
+            self._decoder = core.create_from_bytes(source)
+        elif isinstance(source, torch.Tensor):
+            self._decoder = core.create_from_tensor(source)
+        else:
+            raise TypeError(
+                f"Unknown source type: {type(source)}. "
+                "Supported types are str, bytes and Tensor."
+            )
+
+        core.add_video_stream(self._decoder)
+
+        # TODO: We should either implement specific core library function to
+        # retrieve these values, or replace this with a non-JSON metadata
+        # retrieval.
+        metadata_json = json.loads(core.get_json_metadata(self._decoder))
+        self._num_frames = metadata_json["numFrames"]
+        self._stream_index = metadata_json["bestVideoStreamIndex"]
+
+    def __len__(self) -> int:
+        return self._num_frames
+
+    def __getitem__(self, key: int) -> torch.Tensor:
+        if not isinstance(key, int):
+            raise TypeError(
+                f"Unsupported key type: {type(key)}. Supported type is int."
+            )
+
+        if key < 0:
+            key += self._num_frames
+        if key >= self._num_frames or key < 0:
+            raise IndexError(
+                f"Index {key} is out of bounds; length is {self._num_frames}"
+            )
+
+        return core.get_frame_at_index(
+            self._decoder, frame_index=key, stream_index=self._stream_index
+        )
+
+    def __iter__(self) -> "SimpleVideoDecoder":
+        return self
+
+    def __next__(self) -> torch.Tensor:
+        # TODO: We should distinguish between expected end-of-file and unexpected
+        # runtime error.
+        try:
+            return core.get_next_frame(self._decoder)
+        except RuntimeError:
+            raise StopIteration()
