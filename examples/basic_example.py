de0af91456fd2082671f36405296a202782e5c15
Nicolas Hug <contact@nicolas-hug.com>
2024-06-27 11:18:55 +0100

    More structure for basic example

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -2,38 +2,47 @@
 ==================================================
 Basic Example to use TorchCodec to decode a video.
 ==================================================
+
+A simple example showing how to decode the first few frames of a video  using
+the :class:`~torchcodec.decoders.SimpleVideoDecoder` class.
 """
 
-# A simple example showing how to decode the first few frames of a video.
-# using the :class:`~torchcodec.decoders.SimpleVideoDecoder` class.
 # %%
 import inspect
 import os
 
 from torchcodec.decoders import SimpleVideoDecoder
 
+# %%
 my_path = os.path.abspath(inspect.getfile(inspect.currentframe()))
 video_file_path = os.path.dirname(my_path) + "/../test/resources/nasa_13013.mp4"
 simple_decoder = SimpleVideoDecoder(video_file_path)
-# Since `simple_decoder` is an iterable, you can get the total frame count for
-# the best video stream by calling len().
+# %%
+# You can get the total frame count for the best video stream by calling len().
+
 num_frames = len(simple_decoder)
 print(f"{video_file_path=} has {num_frames} frames")
 
+# %%
 # You can get the decoded frame by using the subscript operator.
+
 first_frame = simple_decoder[0]
 print(f"decoded frame has type {type(first_frame)}")
+
+# %%
 # The shape of the decoded frame is (H, W, C) where H and W are the height
-# and width of the video frame, respectively. C is 3 because we have 3 channels
-# red, green, and blue.
+# and width of the video frame. C is 3 because we have 3 channels red, green,
+# and blue.
+
 print(f"{first_frame.shape=}")
-# The dtype of the decoded frame is uint8.
+# %%
+# The dtype of the decoded frame is ``torch.uint8``.
+
 print(f"{first_frame.dtype=}")
 
+# %%
 # Negative indexes are supported.
 last_frame = simple_decoder[-1]
 print(f"{last_frame.shape=}")
 
 # TODO: add documentation for slices and metadata.
-
-# %%
