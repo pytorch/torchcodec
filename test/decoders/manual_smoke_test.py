a0bb7d8fbcfc62ff7858085b4abec1095be2b8cd
Nicolas Hug <contact@nicolas-hug.com>
2024-07-08 05:37:17 -0700

    Fix smoke tests (#63)
    Summary:
    D59336037 broke the smoke tests because `scan_all_streams_to_update_metadata()` wasn't called before calling `get_frame_at_index()`.
    
    Pull Request resolved: https://github.com/pytorch-labs/torchcodec/pull/63
    
    Reviewed By: ahmadsharif1
    
    Differential Revision: D59459657
    
    Pulled By: NicolasHug
    
    fbshipit-source-id: 6b00484adb52f848361f577c5f6ee6e685d73489

--- test/decoders/manual_smoke_test.py
+++ test/decoders/manual_smoke_test.py
@@ -6,6 +6,7 @@ from torchvision.io.image import write_png
 decoder = torchcodec.decoders._core.create_from_file(
     os.path.dirname(__file__) + "/../resources/nasa_13013.mp4"
 )
+torchcodec.decoders._core.scan_all_streams_to_update_metadata(decoder)
 torchcodec.decoders._core.add_video_stream(decoder, stream_index=3)
 frame = torchcodec.decoders._core.get_frame_at_index(
     decoder, stream_index=3, frame_index=180
