b7ab17c4f317983814da82f51f238f8ee93e0639
Ahmad Sharif <ahmads@meta.com>
2024-07-11 08:43:13 -0700

    [torchcodec] Update functions that return a single frame to return frame and metadata (#74)
    Summary:
    Pull Request resolved: https://github.com/pytorch-labs/torchcodec/pull/74
    
    We now return the frame tensor, frame pts and frame duration for these functions.
    
    Users who use the sampler or SimpleVideoDecoder are not affected.
    
    Note that the pts and duration are returned as Tensors. This is due to a limitation of torch.compile() where it will not accept tuples of non-Tensors:
    
    https://fb.workplace.com/groups/1075192433118967/posts/1461960124442194/?comment_id=1461971881107685
    
    I also removed the `get_frame_with_info_at_index` because now that's redundant with `get_frame_at_index`.
    
    In the proceses I also included VideoDecoderOps.h in VideoDecoderOps.cpp and fixed some inconsistencies between the two that were causing compilation failures.
    
    TODO: I will handle the batch API functions in a subsequent diff.
    
    Reviewed By: scotts, NicolasHug
    
    Differential Revision: D59542194

--- test/decoders/VideoDecoderOpsTest.cpp
+++ test/decoders/VideoDecoderOpsTest.cpp
@@ -37,8 +37,11 @@ TEST(VideoDecoderOpsTest, TestCreateDecoderFromBuffer) {
   size_t length = outputStringStream.str().length();
   at::Tensor decoder = create_from_buffer(buffer, length);
   add_video_stream(decoder);
-  at::Tensor tensor1 = get_next_frame(decoder);
+  auto result = get_next_frame(decoder);
+  at::Tensor tensor1 = std::get<0>(result);
   EXPECT_EQ(tensor1.sizes(), std::vector<long>({270, 480, 3}));
+  EXPECT_EQ(std::get<1>(result).item<double>(), 0);
+  EXPECT_NEAR(std::get<2>(result).item<double>(), 0.033367, 1e-6);
 }
 
 } // namespace facebook::torchcodec
