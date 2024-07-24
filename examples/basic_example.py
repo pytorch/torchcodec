7a02fd61ce4d89c3d82ca9df2f05314817dae46f
Nicolas Hug <nicolashug@fb.com>
2024-07-24 03:36:36 -0700

    oops

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -8,39 +8,19 @@ the :class:`~torchcodec.decoders.SimpleVideoDecoder` class.
 """
 
 # %%
-import inspect
-import os
+import requests
 
+# %%
+# Video is CC0 Licensed, thanks to Coverr for uploading
+# Source: https://www.pexels.com/video/dog-eating-854132/
+video_url = "https://videos.pexels.com/video-files/854132/854132-sd_640_360_25fps.mp4"
+response = requests.get(video_url)
+
+if response.status_code != 200:
+    raise RuntimeError(f"Failed to download video")
+
+# %%
 from torchcodec.decoders import SimpleVideoDecoder
 
-# %%
-my_path = os.path.abspath(inspect.getfile(inspect.currentframe()))
-video_file_path = os.path.dirname(my_path) + "/../test/resources/nasa_13013.mp4"
-simple_decoder = SimpleVideoDecoder(video_file_path)
-
-# %%
-# You can get the total frame count for the best video stream by calling len().
-num_frames = len(simple_decoder)
-print(f"{video_file_path=} has {num_frames} frames")
-
-# %%
-# You can get the decoded frame by using the subscript operator.
-first_frame = simple_decoder[0]
-print(f"decoded frame has type {type(first_frame)}")
-
-# %%
-# The shape of the decoded frame is (H, W, C) where H and W are the height
-# and width of the video frame. C is 3 because we have 3 channels red, green,
-# and blue.
-print(f"{first_frame.shape=}")
-
-# %%
-# The dtype of the decoded frame is ``torch.uint8``.
-print(f"{first_frame.dtype=}")
-
-# %%
-# Negative indexes are supported.
-last_frame = simple_decoder[-1]
-print(f"{last_frame.shape=}")
-
-# TODO_BEFORE_RELEASE: add documentation for slices and metadata.
+decoder = SimpleVideoDecoder(source=response.content)
+decoder[0]
--- src/torchcodec/decoders/_core/video_decoder_ops.py
+++ src/torchcodec/decoders/_core/video_decoder_ops.py
@@ -85,7 +85,6 @@ _get_json_ffmpeg_library_versions = (
 # Functions not related to custom ops, but similar implementation to c++ ops
 # =============================
 def create_from_bytes(video_bytes: bytes) -> torch.Tensor:
-    import warnings
     with warnings.catch_warnings():
         # Ignore warning stating that the underlying video_bytes buffer is
         # non-writable.
