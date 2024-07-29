242f184ca699d9cf5d0ec7f4fa9acade97d6bbc0
Nicolas Hug <contact@nicolas-hug.com>
2024-07-29 11:02:33 +0100

    Ignore

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -4,8 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# noqa: E251,E202
-
 """
 ========================================
 Decoding a video with SimpleVideoDecoder
