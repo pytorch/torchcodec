a99b5e1b0d9b7a85e6570549c036603df08bb533
Nicolas Hug <contact@nicolas-hug.com>
2024-08-01 09:40:06 +0100

    .

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-conda install "ffmpeg=4.4.2" -c conda-forge
+conda install "ffmpeg<5" -c conda-forge
 
 # LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
