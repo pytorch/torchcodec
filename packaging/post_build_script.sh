188b0970a3a4532786fe66caf158cafc95459c3c
Nicolas Hug <contact@nicolas-hug.com>
2024-08-01 09:20:34 +0100

    disable relocate

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -2,4 +2,4 @@
 
 conda install ffmpeg -c conda-forge
 
-LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
+# LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
