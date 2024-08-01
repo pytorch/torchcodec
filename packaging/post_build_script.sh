6f606aa289584dd3ccf36b0f3ab9abae7e0431b2
Nicolas Hug <contact@nicolas-hug.com>
2024-08-01 09:59:39 +0100

    nosudo

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-conda install "ffmpeg<5" -c conda-forge
+yum install epel-release
+yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+yum install ffmpeg
 
 # LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
