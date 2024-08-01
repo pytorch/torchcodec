6d1e7952ac5a8c47e2f039473c8e9653710b5312
Nicolas Hug <contact@nicolas-hug.com>
2024-08-01 11:19:11 +0100

    Add lots of exclude

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -5,4 +5,7 @@ ls
 echo "LS dist"
 ls dist
 echo "auditwheel"
-auditwheel repair --plat manylinux_2_17_x86_64 dist/*
+auditwheel repair --plat manylinux_2_17_x86_64 dist/* --exclude libtorch.so --exclude libtorch_cpu.so --exclude libc10.so --exclude libavutil.so.56 --exclude libavcodec.so.58 --exclude libavformat.so.58 --exclude libavdevice.so.58 --exclude libavfilter.so.7 --exclude libavutil.so.57 --exclude libavcodec.so.59 --exclude libavformat.so.59 --exclude libavdevice.so.59 --exclude libavfilter.so.8 --exclude libavutil.so.58 --exclude libavcodec.so.60 --exclude libavformat.so.60 --exclude libavdevice.so.60 --exclude libavfilter.so.9 --exclude libavutil.so.59 --exclude libavcodec.so.61 --exclude libavformat.so.61 --exclude libavdevice.so.61 --exclude libavfilter.so.10
+
+echo "tree dist"
+tree dist
