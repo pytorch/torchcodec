cab67f360078ac45f059f498acba377a654839d3
Nicolas Hug <contact@nicolas-hug.com>
2024-08-01 09:59:22 +0100

    try more stuff

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-yum install ffmpeg
+sudo yum install epel-release
+sudo yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+sudo yum install ffmpeg
 
 # LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
