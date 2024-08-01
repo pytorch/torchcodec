63d6790c0edbaefedfe59e29424639cc933513df
Nicolas Hug <contact@nicolas-hug.com>
2024-08-01 10:08:16 +0100

    fake smoke test

--- packaging/post_build_script.sh
+++ packaging/post_build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum install epel-release
-yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-yum install ffmpeg
+# yum install epel-release
+# yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+# yum install ffmpeg
 
 # LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" python packaging/wheel/relocate.py
