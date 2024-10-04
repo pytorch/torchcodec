0c916b33c8a53e673117d5e0ba2bb8658eeb5329
Ahmad Sharif <ahmads@fb.com>
2024-10-04 14:43:02 -0700

    .

--- src/torchcodec/decoders/_core/CPUOnlyDevice.cpp
+++ src/torchcodec/decoders/_core/CPUOnlyDevice.cpp
@@ -12,4 +12,12 @@ void maybeInitializeDeviceContext(
   throw std::runtime_error("Unsupported device: " + device.str());
 }
 
+VideoDecoder::DecodedOutput convertAVFrameToDecodedOutputOnDevice(
+    const torch::Device& device,
+    const VideoDecoder::VideoStreamDecoderOptions& options,
+    AVCodecContext* codecContext,
+    VideoDecoder::RawDecodedOutput& rawOutput) {
+  TORCH_CHECK(false, "We should not run device code on CPU")
+}
+
 } // namespace facebook::torchcodec
