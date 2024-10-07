e0dd2536bcdfd475ff43604274109438104e5a49
Ahmad Sharif <ahmads@fb.com>
2024-10-07 07:24:32 -0700

    .

--- src/torchcodec/decoders/_core/CPUOnlyDevice.cpp
+++ src/torchcodec/decoders/_core/CPUOnlyDevice.cpp
@@ -2,6 +2,10 @@
 
 namespace facebook::torchcodec {
 
+// This file is linked with the CPU-only version of torchcodec.
+// So all functions will throw an error because they should only be called if
+// the device is not CPU.
+
 void throwUnsupportedDeviceError(const torch::Device& device) {
   throw std::runtime_error("Unsupported device: " + device.str());
 }
