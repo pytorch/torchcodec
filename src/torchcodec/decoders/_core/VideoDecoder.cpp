c03294bbf95486f3f12bc9bb6746f26ddb3a54da
Nicolas Hug <contact@nicolas-hug.com>
2024-10-24 10:36:52 +0100

    Fix binary search of getFramesDisplayedByTimestamps

--- src/torchcodec/decoders/_core/VideoDecoder.cpp
+++ src/torchcodec/decoders/_core/VideoDecoder.cpp
@@ -1119,21 +1119,14 @@ VideoDecoder::BatchDecodedOutput VideoDecoder::getFramesDisplayedByTimestamps(
 
     auto it = std::lower_bound(
         stream.allFrames.begin(),
-        stream.allFrames.end(),
+        stream.allFrames.end() - 1,
         framePts,
         [&stream](const FrameInfo& info, double framePts) {
           return ptsToSeconds(info.nextPts, stream.timeBase) <= framePts;
         });
     int64_t frameIndex = it - stream.allFrames.begin();
-    // If the frame index is larger than the size of allFrames, that means we
-    // couldn't match the pts value to the pts value of a NEXT FRAME. And
-    // that means that this timestamp falls during the time between when the
-    // last frame is displayed, and the video ends. Hence, it should map to the
-    // index of the last frame.
-    frameIndex = std::min(frameIndex, (int64_t)stream.allFrames.size() - 1);
     frameIndices[i] = frameIndex;
   }
-
   return getFramesAtIndices(streamIndex, frameIndices);
 }
 
