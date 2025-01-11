f62af46dd2b1e539ba5cb5d7ea3801db50c51be7
Scott Schneider <scott.a.s@gmail.com>
2025-01-10 16:29:01 -0800

    Provide constructor param names

--- src/torchcodec/decoders/_core/VideoDecoder.h
+++ src/torchcodec/decoders/_core/VideoDecoder.h
@@ -53,11 +53,11 @@ class VideoDecoder {
   enum class SeekMode { exact, approximate };
 
   // Creates a VideoDecoder from the video at videoFilePath.
-  explicit VideoDecoder(const std::string& videoFilePath, SeekMode);
+  explicit VideoDecoder(const std::string& videoFilePath, SeekMode seek);
 
   // Creates a VideoDecoder from a given buffer. Note that the buffer is not
   // owned by the VideoDecoder.
-  explicit VideoDecoder(const void* buffer, size_t length, SeekMode);
+  explicit VideoDecoder(const void* buffer, size_t length, SeekMode seek);
 
   static std::unique_ptr<VideoDecoder> createFromFilePath(
       const std::string& videoFilePath,
