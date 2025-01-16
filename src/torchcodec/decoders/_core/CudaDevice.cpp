bf68d423d240a7e29a417992a5f75d666fe27d94
Scott Schneider <scott.a.s@gmail.com>
2025-01-16 10:09:31 -0800

    Simple const pointer

--- src/torchcodec/decoders/_core/CudaDevice.cpp
+++ src/torchcodec/decoders/_core/CudaDevice.cpp
@@ -265,17 +265,17 @@ std::optional<const AVCodec*> findCudaCodec(
   throwErrorIfNonCudaDevice(device);
 
   void* i = nullptr;
-  std::unique_ptr<const AVCodec*> codec(new const AVCodec*);
-  while ((*codec = av_codec_iterate(&i)) != nullptr) {
-    if ((*codec)->id != codecId || !av_codec_is_decoder(*codec)) {
+  const AVCodec* codec = nullptr;
+  while ((codec = av_codec_iterate(&i)) != nullptr) {
+    if (codec->id != codecId || !av_codec_is_decoder(codec)) {
       continue;
     }
 
-    const AVCodecHWConfig** config = nullptr;
-    for (int j = 0; (*config = avcodec_get_hw_config(*codec, j)) != nullptr;
+    const AVCodecHWConfig* config = nullptr;
+    for (int j = 0; (config = avcodec_get_hw_config(codec, j)) != nullptr;
          ++j) {
-      if ((*config)->device_type == AV_HWDEVICE_TYPE_CUDA) {
-        return *codec;
+      if (config->device_type == AV_HWDEVICE_TYPE_CUDA) {
+        return codec;
       }
     }
   }
