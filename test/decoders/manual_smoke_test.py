590fe1ce1043e1ef7be0ffa7c19842608c5e4444
Nicolas Hug <contact@nicolas-hug.com>
2025-02-11 14:45:02 +0000

    Remove `streamIndex` parameter from core and ops APIs (#509)

--- test/decoders/manual_smoke_test.py
+++ test/decoders/manual_smoke_test.py
@@ -16,7 +16,5 @@ decoder = torchcodec.decoders._core.create_from_file(
 )
 torchcodec.decoders._core.scan_all_streams_to_update_metadata(decoder)
 torchcodec.decoders._core.add_video_stream(decoder, stream_index=3)
-frame, _, _ = torchcodec.decoders._core.get_frame_at_index(
-    decoder, stream_index=3, frame_index=180
-)
+frame, _, _ = torchcodec.decoders._core.get_frame_at_index(decoder, frame_index=180)
 write_png(frame, "frame180.png")
