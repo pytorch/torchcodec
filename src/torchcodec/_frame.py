b572276b7816224caaed0d3af1ba9829b60c91cc
Nicolas Hug <contact@nicolas-hug.com>
2025-03-03 09:26:20 +0000

    reduce diff

--- src/torchcodec/_frame.py
+++ src/torchcodec/_frame.py
@@ -41,7 +41,6 @@ class Frame(Iterable):
     def __post_init__(self):
         # This is called after __init__() when a Frame is created. We can run
         # input validation checks here.
-
         if not self.data.ndim == 3:
             raise ValueError(f"data must be 3-dimensional, got {self.data.shape = }")
         self.pts_seconds = float(self.pts_seconds)
@@ -79,7 +78,6 @@ class FrameBatch(Iterable):
     def __post_init__(self):
         # This is called after __init__() when a FrameBatch is created. We can
         # run input validation checks here.
-
         if self.data.ndim < 3:
             raise ValueError(
                 f"data must be at least 3-dimensional, got {self.data.shape = }"
