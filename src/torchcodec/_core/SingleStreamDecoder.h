18ad24ff5b3c196abdc120059b321e4e337bf952
Daniel Flores <danielflores3@meta.com>
2025-05-30 10:46:31 -0700

    Update comment to reflect change

--- src/torchcodec/_core/SingleStreamDecoder.h
+++ src/torchcodec/_core/SingleStreamDecoder.h
@@ -121,7 +121,7 @@ class SingleStreamDecoder {
   //
   // Valid values for startSeconds and stopSeconds are:
   //
-  //   [beginStreamSecondsFromContent, maxPtsSecondsFromScan)
+  //   [beginStreamSecondsFromContent, endStreamSecondsFromContent)
   FrameBatchOutput getFramesPlayedInRange(
       double startSeconds,
       double stopSeconds);
