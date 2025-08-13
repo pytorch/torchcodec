e8616cbb14f1e38f920a8f3c1f2c997e94af2580
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2025-08-13 01:47:50 +0000

    Implement abstract per-GPU cache helper class
    This commit introduces abstract per-GPU cache helper class. It allows
    to store and reuse HW contexts which are expensive to create. Class
    is abstract and allows storing different object types.
    
    Co-authored-by: Nicolas Hug <contact@nicolas-hug.com>
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- src/torchcodec/_core/FFMPEGCommon.h
+++ src/torchcodec/_core/FFMPEGCommon.h
@@ -76,6 +76,8 @@ using UniqueSwrContext =
     std::unique_ptr<SwrContext, Deleterp<SwrContext, void, swr_free>>;
 using UniqueAVAudioFifo = std::
     unique_ptr<AVAudioFifo, Deleter<AVAudioFifo, void, av_audio_fifo_free>>;
+using UniqueAVBufferRef =
+    std::unique_ptr<AVBufferRef, Deleterp<AVBufferRef, void, av_buffer_unref>>;
 
 // These 2 classes share the same underlying AVPacket object. They are meant to
 // be used in tandem, like so:
