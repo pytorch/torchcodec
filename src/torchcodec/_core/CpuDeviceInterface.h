8662ee19ed145a831ab6952a4262fb270bc9b99c
Scott Schneider <scott.a.s@gmail.com>
2025-09-05 16:31:34 -0400

    Refactor context structs to use constructors (#869)

--- src/torchcodec/_core/CpuDeviceInterface.h
+++ src/torchcodec/_core/CpuDeviceInterface.h
@@ -43,11 +43,19 @@ class CpuDeviceInterface : public DeviceInterface {
       const UniqueAVFrame& avFrame);
 
   struct SwsFrameContext {
-    int inputWidth;
-    int inputHeight;
-    AVPixelFormat inputFormat;
-    int outputWidth;
-    int outputHeight;
+    int inputWidth = 0;
+    int inputHeight = 0;
+    AVPixelFormat inputFormat = AV_PIX_FMT_NONE;
+    int outputWidth = 0;
+    int outputHeight = 0;
+
+    SwsFrameContext() = default;
+    SwsFrameContext(
+        int inputWidth,
+        int inputHeight,
+        AVPixelFormat inputFormat,
+        int outputWidth,
+        int outputHeight);
     bool operator==(const SwsFrameContext&) const;
     bool operator!=(const SwsFrameContext&) const;
   };
