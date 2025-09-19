c06aa947a4917c4c303a2aa258639c24fbcf8fc9
Scott Schneider <scott.a.s@gmail.com>
2025-09-19 08:53:18 -0700

    Cuda device convert frames function

--- src/torchcodec/_core/CudaDeviceInterface.h
+++ src/torchcodec/_core/CudaDeviceInterface.h
@@ -28,10 +28,7 @@ class CudaDeviceInterface : public DeviceInterface {
       const FrameDims& outputDims) override;
 
   void convertAVFrameToFrameOutput(
-      const VideoStreamOptions& videoStreamOptions,
-      const AVRational& timeBase,
       UniqueAVFrame& avFrame,
-      const FrameDims& outputDims,
       FrameOutput& frameOutput,
       std::optional<torch::Tensor> preAllocatedOutputTensor =
           std::nullopt) override;
